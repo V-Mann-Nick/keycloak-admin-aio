1ac1f6d9a5305c2d3fe508878e5c0eb2eb0450ba
Nicklas Sedlock <nicklas@delphai.com>
2021-09-22 11:14:07 +0200

    feat: grant_type=client_credentials flow allowed
    - can authenticate with `client_credentials`
    - added two classmethods `with_client_credentials` and `with_password`
      for instantiating with different flows

--- keycloak_admin_aio/keycloak_admin_aio.py
+++ keycloak_admin_aio/keycloak_admin_aio.py
@@ -1,6 +1,6 @@
 import asyncio
 from datetime import datetime, timedelta
-from typing import Literal
+from typing import Literal, Optional
 
 import httpx
 
@@ -20,10 +20,25 @@ class KeycloakAdmin:
     client_scopes: ClientScopes
 
     def __init__(
-        self, server_url: str, username: str, password: str, realm: str = "master"
+        self,
+        server_url: str,
+        grant_type: Literal["client_credentials", "password"],
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        client_id: Optional[str] = None,
+        client_secret: Optional[str] = None,
+        realm: str = "master",
     ):
+        allowed_grant_types = ["client_credentials", "password"]
+        if not any([grant_type == allowed for allowed in allowed_grant_types]):
+            raise Exception(f"'grant_type' needs to be in '{allowed_grant_types}'")
         self._realm = realm
         self._username = username
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._password = password
+        self._server_url = server_url
+        self._grant_type = grant_type
 
         async def raise_for_status_hook(response: httpx.Response):
             response.raise_for_status()
@@ -31,32 +46,70 @@ class KeycloakAdmin:
         self.__connection = httpx.AsyncClient(
             event_hooks={"response": [raise_for_status_hook]}
         )
-        self.__server_url = server_url
-        self.__password = password
         self.__access_token = None
         self.__refresh_token = None
         self.__set_keycloak_resources()
+
+    @classmethod
+    def with_client_credentials(
+        cls, server_url: str, client_id: str, client_secret: str, realm: str = "master"
+    ):
+        return cls(
+            server_url,
+            grant_type="client_credentials",
+            client_id=client_id,
+            client_secret=client_secret,
+            realm=realm,
+        )
+
+    @classmethod
+    def with_password(
+        cls,
+        server_url: str,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        client_id: Optional[str] = "admin-cli",
+        realm: str = "master",
+    ):
+        return cls(
+            server_url,
+            grant_type="password",
+            client_id=client_id,
+            username=username,
+            password=password,
+            realm=realm,
+        )
 
     @property
     def realm(self):
         return self._realm
 
-    @realm.setter
-    def realm(self, new_realm):
-        self.close_sync()
-        self.__init__(self.__server_url, self._username, self.__password, new_realm)
-
     @property
     def username(self):
         return self._username
 
-    @username.setter
-    def username(self, new_username):
-        self.close_sync()
-        self.__init__(self.__server_url, new_username, self.__password, self._realm)
+    @property
+    def client_id(self):
+        return self._client_id
+
+    @property
+    def client_secret(self):
+        return self.client_secret
+
+    @property
+    def password(self):
+        return self._password
+
+    @property
+    def server_url(self):
+        return self._server_url
+
+    @property
+    def grant_type(self):
+        return self._grant_type
 
     def get_url(self):
-        return f"{self.__server_url}/admin/realms/{self._realm}"
+        return f"{self._server_url}/admin/realms/{self._realm}"
 
     async def get_access_token(self):
         if not self.__access_token or not self.__refresh_token:
@@ -91,33 +144,46 @@ class KeycloakAdmin:
         self,
         token_type: Literal["access_token", "refresh_token"],
     ):
-        url = f"{self.__server_url}/{URL_TOKEN}".format(**{"realm-name": self._realm})
+        token_endpoint = f"{self._server_url}/{URL_TOKEN}".format(
+            **{"realm-name": self._realm}
+        )
         headers = httpx.Headers({"Content-Type": "application/x-www-form-urlencoded"})
+        payload = {"client_id": self._client_id}
         if token_type == "refresh_token":
-            payload = {
-                "grant_type": "refresh_token",
-                "refresh_token": self.__refresh_token,
-            }
+            payload.update(
+                {
+                    "grant_type": "refresh_token",
+                    "refresh_token": self.__refresh_token,
+                }
+            )
         elif token_type == "access_token":
-            payload = {
-                "grant_type": "password",
-                "username": self._username,
-                "password": self.__password,
-            }
+            grant_type = self.grant_type
+            payload.update(
+                {
+                    "grant_type": grant_type,
+                }
+            )
+            if grant_type == "client_credentials":
+                payload.update(
+                    {"client_id": self._client_id, "client_secret": self._client_secret}
+                )
+            else:
+                payload.update({"username": self._username, "password": self._password})
         else:
             raise Exception(
                 "Positional argument 'token_type' needs to be either 'access_token' or 'refresh_token'"
             )
-        payload["client_id"] = "admin-cli"
-        response = await self.__connection.post(url, data=payload, headers=headers)
+        response = await self.__connection.post(
+            token_endpoint, data=payload, headers=headers
+        )
         token = response.json()
         self.__access_token = token["access_token"]
-        self.__refresh_token = token["refresh_token"]
+        self.__refresh_token = token.get("refresh_token")
         self.access_token_expire = datetime.now() + timedelta(
             seconds=token["expires_in"] - 10
         )
         self.refresh_token_expire = datetime.now() + timedelta(
-            seconds=token["refresh_expires_in"] - 10
+            seconds=token.get("refresh_expires_in", 0) - 10
         )
 
     async def __get_connection(self):
