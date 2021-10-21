6198779ecd78c1930362754d04d033042a738bc0
Nicklas Sedlock <nicklas@delphai.com>
2021-10-21 17:29:41 +0200

    feat: incomplete authentication resource
    - `/authentication/required-actions`

--- keycloak_admin_aio/types.py
+++ keycloak_admin_aio/types.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from keycloak_admin_aio.lib.data_class import DataClass
-from typing import Any, Optional
+from typing import Any, Literal, Optional
 
 
 @dataclass
@@ -31,7 +31,7 @@ class ClientMappingsRepresentation(DataClass):
 @dataclass
 class MappingsRepresentation(DataClass):
     clientMappings: Optional[dict[str, ClientMappingsRepresentation]] = None
-    realmMappings: Optional[RoleRepresentation] = None
+    realmMappings: Optional[list[RoleRepresentation]] = None
 
 
 @dataclass
@@ -210,3 +210,53 @@ class ClientRepresentation(DataClass):
     standardFlowEnabled: Optional[bool] = None
     surrogateAuthRequired: Optional[bool] = None
     webOrigins: Optional[list[str]] = None
+
+
+@dataclass
+class RequiredActionProviderRepresentation(DataClass):
+    alias: Optional[str] = None
+    config: Optional[dict[str, Any]] = None
+    defaultAction: Optional[bool] = None
+    enabled: Optional[bool] = None
+    name: Optional[str] = None
+    priority: Optional[int] = None
+    providerId: Optional[str] = None
+
+
+ResourceType = Literal[
+    "REALM",
+    "REALM_ROLE",
+    "REALM_ROLE_MAPPING",
+    "REALM_SCOPE_MAPPING",
+    "AUTH_FLOW",
+    "AUTH_EXECUTION_FLOW",
+    "AUTH_EXECUTION",
+    "AUTHENTICATOR_CONFIG",
+    "REQUIRED_ACTION",
+    "IDENTITY_PROVIDER",
+    "IDENTITY_PROVIDER_MAPPER",
+    "PROTOCOL_MAPPER",
+    "USER",
+    "USER_LOGIN_FAILURE",
+    "USER_SESSION",
+    "USER_FEDERATION_PROVIDER",
+    "USER_FEDERATION_MAPPER",
+    "GROUP",
+    "GROUP_MEMBERSHIP",
+    "CLIENT",
+    "CLIENT_INITIAL_ACCESS_MODEL",
+    "CLIENT_ROLE",
+    "CLIENT_ROLE_MAPPING",
+    "CLIENT_SCOPE",
+    "CLIENT_SCOPE_MAPPING",
+    "CLIENT_SCOPE_CLIENT_MAPPING",
+    "CLUSTER_NODE",
+    "COMPONENT",
+    "AUTHORIZATION_RESOURCE_SERVER",
+    "AUTHORIZATION_RESOURCE",
+    "AUTHORIZATION_SCOPE",
+    "AUTHORIZATION_POLICY",
+    "CUSTOM",
+]
+
+OperationType = Literal["CREATE", "DELETE", "ACTION", "UPDATE"]
