37f08a731b677c36fddf1d8f224266e66b793f35
Nicklas Sedlock <nicklas@delphai.com>
2021-11-03 14:56:38 +0100

    refactor: created separate module for httpx default args

---
+++ keycloak_admin_aio/lib/httpx_args.py
@@ -0,0 +1,12 @@
+import httpx
+
+
+async def raise_for_status_hook(response: httpx.Response):
+    response.raise_for_status()
+
+
+httpx_default_args = {"event_hooks": {"response": [raise_for_status_hook]}}
+
+
+def merge_with_default_httpx_args(user_args: dict) -> dict:
+    return {**httpx_default_args, **user_args}
