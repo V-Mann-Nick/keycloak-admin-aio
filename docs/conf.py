507b9aef4e5d41c496e696b5ad51c4683df9780a
Nicklas Sedlock <nicklas@delphai.com>
2021-11-03 17:02:19 +0100

    chore: added github action step for building and releasing docs
    - renamend `bumpversion` to `master.yml`
    - added `sphinx.ext.githubpages` to sphinx `conf.py`
    - added step for building and releasing docs to github pages

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
 
 
 # -- Project information -----------------------------------------------------
@@ -27,7 +28,7 @@ author = "Nicklas Sedlock"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.githubpages"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
