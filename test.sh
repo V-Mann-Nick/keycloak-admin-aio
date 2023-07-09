133c0c02f2f3b5662be261cd12c767bf03e49d9a
Nicklas Sedlock <nicklas.sedlock@posteo.net>
2023-07-09 20:27:21 +0200

    test: provide test script which spins up keycloak and runs tests

---
+++ test.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+if command -v podman >/dev/null 2>&1; then
+  container=podman
+fi 
+
+if command -v docker >/dev/null 2>&1; then
+  container=docker
+fi
+
+if [ -z "$container" ]; then
+  echo -e '❓ No container runtime found. Requires either docker or podman.'
+  exit 1
+fi
+
+if ! command -v curl >/dev/null 2>&1; then
+  echo -e '❓ curl is required to test Keycloak'
+  exit 1
+fi
+
+# Start keycloak detached
+echo -e '🚀 Starting Keycloak'
+container_id=$($container run \
+-d \
+--env KEYCLOAK_ADMIN=testing \
+--env KEYCLOAK_ADMIN_PASSWORD=testing \
+--env KC_HEALTH_ENABLED=true \
+-p 8080:8080 \
+quay.io/keycloak/keycloak:latest \
+start-dev
+)
+
+# Cleanup container on exit
+function cleanup() {
+  echo -e '🧹 Stopping Keycloak'
+  $container rm -f $container_id >/dev/null
+}
+trap cleanup EXIT
+
+# Poll Keycloak for 1m until it's ready otherwise exit with error
+is_ready=false
+for i in {1..60}; do
+  if curl -s http://localhost:8080/health >/dev/null; then
+    is_ready=true
+    echo -e '✅ Keycloak is ready'
+    break
+  fi
+  echo -e '🕑 Waiting for Keycloak to start'
+  sleep 1
+done
+if [ "$is_ready" = false ]; then
+  echo -e '❌ Keycloak failed to start'
+  exit 1
+fi
+
+# Run tests
+echo -e '🧪 Running tests'
+poetry run poe pytest
